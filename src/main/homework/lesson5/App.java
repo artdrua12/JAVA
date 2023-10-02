b86a051fc97e485a05c9ef5f302b5a1f71e99d21
artdrua <pasechnyy@proitr.ru>
2023-10-02 23:20:36 +0300

    lesson5

---
+++ src/main/homework/lesson5/App.java
@@ -0,0 +1,45 @@
+package main.homework.lesson5;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        int twoArray[][] = new int[2][2];
+        for (int i = 0; i < twoArray.length; i++) {
+            for (int j = 0; j < twoArray[i].length; j++)
+                twoArray[i][j] = (int) (Math.random() * 10);
+        }
+        System.out.println(Arrays.deepToString(twoArray));
+        // Задача1
+        Scanner scaner = new Scanner(System.in);
+        System.out.println("Введите число которое будет добавлено к элементам массива");
+        int value = scaner.nextInt();
+        int summa = 0;
+        for (int i = 0; i < twoArray.length; i++) {
+            for (int j = 0; j < twoArray[i].length; j++) {
+                twoArray[i][j] += value;
+                summa += twoArray[i][j];
+            }
+
+        }
+        System.out.println("Задача 1 \n" + Arrays.deepToString(twoArray));
+        System.out.println("Cумма элементов = " + summa);
+
+        // Задача2
+        char chessArray[][] = new char[8][8];
+        for (int i = 0; i < chessArray.length; i++) {
+            for (int j = 0; j < chessArray[i].length; j++)
+                if ((i + j) % 2 == 0) {
+                    chessArray[i][j] = 'W';
+                } else {
+                    chessArray[i][j] = 'B';
+                }
+        }
+        System.out.println("Задача 2");
+        for (int i = 0; i < chessArray.length; i++) {
+            System.out.println(Arrays.toString(chessArray[i]));
+        }
+
+    }
+}
