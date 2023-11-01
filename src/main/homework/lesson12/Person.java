b26a1432e47657137500966fc25bea253eb21b94
artdrua <pasechnyy@proitr.ru>
2023-11-02 00:06:41 +0300

    without test

--- src/main/homework/lesson12/Person.java
+++ src/main/homework/lesson12/Person.java
@@ -7,12 +7,18 @@ public class Person {
     public Person() {
     }
 
-    static void checkPerson(String login, String password, String confirmPassword) throws WrongLogin {
+    static void checkPerson(String login, String password, String confirmPassword) throws WrongLogin, WrongPassword {
         if (login.length() >= 20) {
             throw new WrongLogin("Длина не должна быть более 20 символов");
         }
         if (Pattern.matches("[a-zA-Zа-яА-Я]{0,}\\s+[a-zA-Zа-яА-Я]{0,}", login)) {
             throw new WrongLogin("Логин не должен содержать пробелов");
+        }
+
+        if (password.length() >= 20 || Pattern.matches("[a-zA-Zа-яА-Я]{0,}\\s+[a-zA-Zа-яА-Я]{0,}", password)
+                || !Pattern.matches("[a-zA-Zа-яА-Я]{0,}\\d+[a-zA-Zа-яА-Я]{0,}", password)
+                || !password.equals(confirmPassword)) {
+            throw new WrongPassword();
         }
     }
 
