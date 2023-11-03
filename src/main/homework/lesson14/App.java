de0872d240bf053f67109d1350f4dfb558a37919
Martynav.cniitu <martynav.cniitu@gmail.com>
2023-11-03 16:54:51 +0300

    7634

--- src/main/homework/lesson14/App.java
+++ src/main/homework/lesson14/App.java
@@ -1,5 +1,6 @@
 package main.homework.lesson14;
 
+import java.util.LinkedList;
 import java.util.Scanner;
 
 public class App {
@@ -11,11 +12,40 @@ public class App {
         scaner.close();
 
         // Задание2
-        Task2.add("Мишка");
-        Task2.add("Лисичка");
-        Task2.add("Зайчик");
+        Task2.add("Bear");
+        Task2.add("Fox");
+        Task2.add("Rabbit");
         System.out.println(Task2.show());
         Task2.remove();
         System.out.println(Task2.show());
+
+        // Задание3
+        LinkedList<Student> students = new LinkedList<Student>();
+        Student student1 = new Student("Vasa", 1, 351002, new int[] { 3, 4, 5, 1 });
+        Student student2 = new Student("Ola", 2, 740102, new int[] { 1, 2, 5, 1 });
+        Student student3 = new Student("Sasa", 3, 351002, new int[] { 4, 4, 5, 1 });
+        Student student4 = new Student("Ana", 2, 740102, new int[] { 3, 2, 2, 2 });
+        Student student5 = new Student("Kola", 3, 351002, new int[] { 3, 1, 2, 1 });
+        Student student6 = new Student("Ira", 1, 740102, new int[] { 2, 1, 3, 1 });
+        Student student7 = new Student("Oleg", 1, 740102, new int[] { 3, 4, 5, 1 });
+
+        students.add(student1);
+        students.add(student2);
+        students.add(student3);
+        students.add(student4);
+        students.add(student5);
+        students.add(student6);
+        students.add(student7);
+
+        System.out.println(students.toString());
+
+        // for (Student std : students) {
+        //     if (std.checkStudent()) {
+        //         int curse = std.getCurse();
+        //         std.setCurse(curse + 1);
+        //     } else {
+        //         students.remove(std);
+        //     }
+        // }
     }
 }
--- src/main/homework/lesson14/Student.java
+++ src/main/homework/lesson14/Student.java
@@ -1,8 +1,54 @@
 package main.homework.lesson14;
 
 public class Student {
-    public String name;
-    public int curse;
-    public int grupa;
+    private String name;
+    private int curse;
+    private int grupa;
+    private int[] grade;
 
+    public Student(String name, int curse, int grupa, int[] grade) {
+        this.name = name;
+        this.curse = curse;
+        this.grupa = grupa;
+        this.grade = grade;
+    }
+
+    public int[] getGrade() {
+        return grade;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getCurse() {
+        return curse;
+    }
+
+    public int getGrupa() {
+        return grupa;
+    }
+
+    public void setCurse(int curse) {
+        this.curse = curse;
+    }
+
+    public int average(int[] array) {
+
+        int summa = 0;
+        for (int i = 0; i < array.length; i++) {
+            summa += array[i];
+        }
+        return Math.round(summa / array.length);
+
+    }
+
+    public Boolean checkStudent() {
+        int grade = this.average(this.grade);
+        if (grade < 3) {
+            return false;
+        } else {
+            return true;
+        }
+    }
 }
