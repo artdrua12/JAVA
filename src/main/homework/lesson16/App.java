f199f053550549a29905497bfda398073570311c
Martynav.cniitu <martynav.cniitu@gmail.com>
2023-11-14 15:30:00 +0300

    fix

--- src/main/homework/lesson16/App.java
+++ src/main/homework/lesson16/App.java
@@ -1,15 +1,9 @@
 package main.homework.lesson16;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-import java.util.Optional;
 import java.util.Scanner;
-import java.util.Set;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
