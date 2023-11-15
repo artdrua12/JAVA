a6fbda2f71202fdc34591f925527c13ab7caea75
Martynav.cniitu <martynav.cniitu@gmail.com>
2023-11-15 17:54:51 +0300

    dom

--- src/main/homework/lesson17/App.java
+++ src/main/homework/lesson17/App.java
@@ -1,13 +1,28 @@
 package main.homework.lesson17;
 
-import com.fasterxml.jackson.core.JsonParser;
+import java.io.File;
+import java.io.IOException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.w3c.dom.Document;
+import org.xml.sax.SAXException;
 
 public class App {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SAXException, IOException {
         System.err.println(17);
+        // https://www.baeldung.com/java-xerces-dom-parsing
 
-        // XmlMapper xmlMapper = new XmlMapper();
-        // Flower poppy = xmlMapper.readValue(xml, Flower.class);
+        try {
+            DocumentBuilder builder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
+            Document doc = builder.parse(new File("Java/src/main/homework/lesson17/employees.xml"));
+            doc.getDocumentElement().normalize();
+        } catch (ParserConfigurationException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
 
     }
 }
