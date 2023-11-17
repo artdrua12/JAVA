fc20ce1f79789a90638d163cfc0086a12dd154db
Martynav.cniitu <martynav.cniitu@gmail.com>
2023-11-17 09:56:30 +0300

    123

--- src/main/homework/lesson17/App.java
+++ src/main/homework/lesson17/App.java
@@ -1,25 +1,36 @@
 package main.homework.lesson17;
 
-import java.util.List;
+import java.io.IOException;
 
+import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.parsers.SAXParser;
 import javax.xml.parsers.SAXParserFactory;
 
-import main.homework.lesson17.dom.XMLParserDom;
+import org.xml.sax.SAXException;
+
+import main.homework.lesson17.sax.XMLParserSax.BaeldungHandler;
 
 public class App {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ParserConfigurationException, SAXException, IOException {
 
         // получение номера работы
-        int numWork = Entrance.enter.get();
-        // проверка нужен ли выход
-        if (numWork == 3)
-            return;
+        // int numWork = Entrance.enter.get();
+        // // проверка нужен ли выход
+        // if (numWork == 3)
+        // return;
 
-        List<String> list = XMLParserDom.parser("Java/src/main/homework/lesson17/xml.xml", "list");
-        for (String l : list) {
-            System.out.println(l);
-        }
+        // List<String> list =
+        // XMLParserDom.parser("Java/src/main/homework/lesson17/xml.xml", "list");
+        // for (String l : list) {
+        // System.out.println(l);
+        // }
+
+        SAXParserFactory factory = SAXParserFactory.newInstance();
+        SAXParser saxParser = factory.newSAXParser();
+
+        BaeldungHandler baeldungHandler = new BaeldungHandler();
+        saxParser.parse("Java/src/main/homework/lesson17/baeldung.xml", new BaeldungHandler());
+        System.out.println("bd " + baeldungHandler.getWebsite().toString());
 
     }
 
--- src/main/homework/lesson17/sax/XMLParserSAX.java
+++ src/main/homework/lesson17/sax/XMLParserSAX.java
@@ -1,45 +1,66 @@
 package main.homework.lesson17.sax;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
 
 import org.xml.sax.Attributes;
 import org.xml.sax.SAXException;
 import org.xml.sax.helpers.DefaultHandler;
 
-public static class XMLParserSAX extends DefaultHandler {
+public class XMLParserSax {
+    public static void main(String[] args) throws ParserConfigurationException, SAXException, IOException {
+        SAXParserFactory factory = SAXParserFactory.newInstance();
+        SAXParser saxParser = factory.newSAXParser();
 
-    private StringBuilder elementValue;
-    public List<String> list = new ArrayList<>();
+        BaeldungHandler baeldungHandler = new BaeldungHandler();
+        saxParser.parse("Java/src/main/homework/lesson17/baeldung.xml", baeldungHandler);
+        List<String> bd = baeldungHandler.getWebsite();
+        System.out.println("bd " + bd.toString());
+    }
 
-    public static void parser() {
+    public static class BaeldungHandler extends DefaultHandler {
 
-    @Override
-    public void characters(char[] ch, int start, int length) {
-        if (elementValue == null) {
-            elementValue = new StringBuilder();
-        } else {
-            elementValue.append(ch, start, length);
+        private StringBuilder elementValue;
+        public List<String> list = new ArrayList<>();
+
+        @Override
+        public void characters(char[] ch, int start, int length) throws SAXException {
+            if (elementValue == null) {
+                elementValue = new StringBuilder();
+            } else {
+                elementValue.append(ch, start, length);
+            }
+        }
+
+        @Override
+        public void startDocument() throws SAXException {
+
+        }
+
+        @Override
+        public void startElement(String uri, String localName, String qName, Attributes attributes)
+                throws SAXException {
+            if (qName.equalsIgnoreCase("title")) {
+                elementValue = new StringBuilder();
+            }
+        }
+
+        @Override
+        public void endElement(String uri, String localName, String qName) throws SAXException {
+            if (qName.equalsIgnoreCase("title")) {
+                list.add(elementValue.toString());
+            }
+        }
+
+        public List<String> getWebsite() {
+            // return (Baeldung) website;
+            return list;
         }
     }
 
-    @Override
-    public void startElement(String uri, String localName, String qName, Attributes attributes) {
-        if (qName.equalsIgnoreCase("title")) {
-            elementValue = new StringBuilder();
-        }
-    }
-
-    @Override
-    public void endElement(String uri, String localName, String qName) throws SAXException {
-        if (qName.equalsIgnoreCase("title")) {
-            list.add(elementValue.toString());
-        }
-    }
-
-    public List<String> getWebsite() {
-        // return (Baeldung) website;
-        return list;
-
-    }
 }
