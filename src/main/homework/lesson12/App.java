a93211c4572a48ac20b0b877eb6409f66c148d78
Martynav.cniitu <martynav.cniitu@gmail.com>
2023-12-11 11:57:27 +0300

    С заданиями

--- src/main/homework/lesson12/App.java
+++ src/main/homework/lesson12/App.java
@@ -1,10 +1,23 @@
 package main.homework.lesson12;
 
+// Задача 1:
+// Создать класс, в котором будет статический метод. Этот метод принимает на вход три
+// параметра: login, password, confirmPassword. Все поля имеют тип данных String. Длина
+// login должна быть меньше 20 символов и не должен содержать пробелы. Если login не
+// соответствует этим требованиям, необходимо выбросить WrongLoginException. Длина
+// password должна быть меньше 20 символов, не должен содержать пробелом и должен
+// teachmeskills.by
+// содержать хотя бы одну цифру. Также password и confirmPassword должны быть равны.
+// Если password не соответствует этим требованиям, необходимо выбросить
+// WrongPasswordException. WrongPasswordException и WrongLoginException -
+// пользовательские классы исключения с двумя конструкторами – один по умолчанию,
+// второй принимает сообщение исключения и передает его в конструктор класса Exception.
+// Метод возвращает true, если значения верны, false в противном случае.
 public class App {
     public static void main(String[] args) {
         try {
             Person.checkPerson("Oleg", "2Fjdd", "2Fjdd");
-             System.out.println("Проверка пройдена");
+            System.out.println("Проверка пройдена");
         } catch (WrongLogin e) {
             System.out.println(e);
         } catch (WrongPassword e) {
