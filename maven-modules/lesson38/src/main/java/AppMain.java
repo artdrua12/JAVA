d6bdf59d7efe15a4a6b2d69aee8f546be869106e
Alexandr <artdrua12gmail.com>
2024-02-08 17:08:38 +0300

    добавлен источник

--- maven-modules/lesson38/src/main/java/AppMain.java
+++ maven-modules/lesson38/src/main/java/AppMain.java
@@ -3,6 +3,7 @@ import service.EmployeeService;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 // Создание приложения на базе Spring (не Spring BOOT) gjlrk.xbnm АОП (создание аспектов)
+// https://www.digitalocean.com/community/tutorials/spring-aop-example-tutorial-aspect-advice-pointcut-joinpoint-annotations
 public class AppMain {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("spring.xml");
